cb2879dbcc3986a837e78ce9a3bf9cdef30050b9
Ryo Matsumoto (rako) <293704661+rako-biz@users.noreply.github.com>
2026-07-09 10:54:31 +0900

    S1 variable_reassigned_int after test

--- src/test/java/org/docksidestage/javatry/basic/Step01VariableTest.java
+++ src/test/java/org/docksidestage/javatry/basic/Step01VariableTest.java
@@ -68,6 +68,7 @@ public class Step01VariableTest extends PlainTestCase {
         sea = land;
         land++;
         log(sea); // your answer? => 415 プリミティブ型のint型なので、L68で値渡しをしてsea=land=415となり、landのみのインクリメントになるのでseaにはコピーされた415になる。
+        // after test => 415 合っているみたい。landの方もインクリメント後の値を見たら416だった。
     }
 
     /** Same as the previous method question. (前のメソッドの質問と同じ) */
