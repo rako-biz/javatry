fbccd2e282b1230d451acfcbdafc640442feb9a3
Ryo Matsumoto (rako) <293704661+rako-biz@users.noreply.github.com>
2026-07-31 11:24:49 +0900

    S1 variable_method_argument_mutable_methodcall answer

--- src/test/java/org/docksidestage/javatry/basic/Step01VariableTest.java
+++ src/test/java/org/docksidestage/javatry/basic/Step01VariableTest.java
@@ -213,7 +213,7 @@ public class Step01VariableTest extends PlainTestCase {
         StringBuilder sea = new StringBuilder("harbor");
         int land = 415;
         helpMethodArgumentMethodcall(sea, land);
-        log(sea); // your answer? => 
+        log(sea); // your answer? => harbor 上記の問題と一緒に見えるが、String.valueofがなかったり、seaにconcatではなくappendしているのは意味がありそう？
     }
 
     private void helpMethodArgumentMethodcall(StringBuilder sea, int land) {
