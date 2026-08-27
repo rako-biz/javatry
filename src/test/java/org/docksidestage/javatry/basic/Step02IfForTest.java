c1c9241e10d08e7953ef298981aab3f0f8ca9621
Ryo Matsumoto (rako) <293704661+rako-biz@users.noreply.github.com>
2026-08-27 11:52:41 +0900

    S2 for_listforeach_basic answer

--- src/test/java/org/docksidestage/javatry/basic/Step02IfForTest.java
+++ src/test/java/org/docksidestage/javatry/basic/Step02IfForTest.java
@@ -180,7 +180,7 @@ public class Step02IfForTest extends PlainTestCase {
             }
         });
         String sea = sb.toString();
-        log(sea); // your answer? => 
+        log(sea); // your answer? => dockside 最初はlength=0なのでiが含まれるまで飛ばしてiが含まれるようになったらその要素をsbに付け足してlength>0になるので戻って出力する
     }
 
     // ===================================================================================
